5bcb107747d4dd02a23f2a3c028cb2cf3d218c5a
CakeOrRiot <pozdnyakovmaks99@mail.ru>
2022-12-01 10:10:52 +0500

    Day01

--- 2022/day01/src/main.rs
+++ 2022/day01/src/main.rs
@@ -1,8 +1,47 @@
-use rand::Rng;
-use std::io;
+use std::fs;
 
-fn solve_1(file: String) -> String {
-    return "".to_string();
+const FILE_PATH: &str = "input.txt";
+
+fn solve_1() -> i32 {
+    let contents = fs::read_to_string(FILE_PATH).expect("Unable to read file");
+    let split: Vec<&str> = contents.split('\n').collect();
+    let mut cur_sum = 0;
+    let mut max_sum = 0;
+    for cur in &split[1..] {
+        let x = *cur;
+        if x.is_empty() {
+            max_sum = std::cmp::max(max_sum, cur_sum);
+            cur_sum = 0;
+        } else {
+            cur_sum += x.parse::<i32>().unwrap();
+        }
+    }
+
+    return max_sum;
 }
 
-fn main() {}
+fn solve_2() -> i32 {
+    let contents = fs::read_to_string(FILE_PATH).expect("Unable to read file");
+    let split: Vec<&str> = contents.split('\n').collect();
+    let mut cur_sum = 0;
+    let mut max_sum = 0;
+    let mut calories = Vec::new();
+    for cur in &split[1..] {
+        let x = *cur;
+        if x.is_empty() {
+            max_sum = std::cmp::max(max_sum, cur_sum);
+            calories.push(cur_sum);
+            cur_sum = 0;
+        } else {
+            cur_sum += x.parse::<i32>().unwrap();
+        }
+    }
+    calories.sort();
+    let n = calories.len();
+    return calories[n - 1] + calories[n - 2] + calories[n - 3];
+}
+
+fn main() {
+    print!("{}\n", solve_1());
+    print!("{}\n", solve_2());
+}
